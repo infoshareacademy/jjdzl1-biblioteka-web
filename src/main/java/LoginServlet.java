38db679d79b8dad83faf8ca7cc3a32c758153ff4
Łukasz Bezłada <bezi@o2.pl>
2019-02-05 20:15:53 +0100

    little corection

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/index.html/LoginServlet")
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private final String userID = "admin";

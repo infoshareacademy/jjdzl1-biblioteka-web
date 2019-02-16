2c010c83c449b91a7ef7b3499a10507692657624
Lukasz Bezlada <bezi@o2.pl>
2019-02-16 19:41:17 +0100

    Little correction

--- src/main/java/com/infoshare/AuthenticationFilter.java
+++ src/main/java/com/infoshare/AuthenticationFilter.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/AuthenticationFilter")
+@WebFilter(value = "/AuthenticationFilter", urlPatterns = "*")
 public class AuthenticationFilter implements Filter {
 
     private ServletContext context;
